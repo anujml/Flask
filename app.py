e725c214fc0a03a506ca3fd95c8287ad08f4999d
anujml <131592463+anujml@users.noreply.github.com>
2023-11-01 13:46:56 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import os
+import datetime
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+
+
+app = Flask(__name__)
+client = MongoClient("mongodb+srv://Brock1234:Brock1234@cluster0.y5ykvso.mongodb.net/")
+app.db = client.microblog
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        entry_content = request.form.get("content")
+        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+        
+        # Correct way to insert a document using insert_one
+        app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+        
+    entries_with_date = [
+        (
+            entry["content"],
+            entry["date"],
+            datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+        )
+        for entry in app.db.entries.find({})
+    ]
+    return render_template("home.html", entries=entries_with_date)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
